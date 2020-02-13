c0ea89904c3b447186e312a68618053a166c6ec5
VanshikaP <vanshikapundir92@gmail.com>
2020-02-13 14:50:43 -0800

    MVP

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,13 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/VanshikaP')
+.then(response => {
+  console.log(response.data);
+  })
+  .catch(error => {
+    console.log('Got error ', error);
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +31,49 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const friendsArray = [
+  'VanshikaP', 
+  'ajkizer', 
+  'danielkyman', 
+  'stgary',
+  'AhmedBarre10',
+  'Callmich',
+  'idongessien'
+];
+console.log('friends: ', friendsArray);
+friendsArray.forEach(githubHandle =>{
+  axios.get('https://api.github.com/users/' + githubHandle)
+  .then(response => {
+    document.querySelector('.cards').append(createCard(response.data));
+  })
+.catch(error => {
+  console.log('Fetch operation failed, ', error);
+})
+});
+
+let followersArray = [];
+axios.get('https://api.github.com/users/VanshikaP/followers')
+.then(response => {
+  response.data.forEach(r => {
+    followersArray.push(r.login);
+  })
+}).catch(error => {
+  console.log('Failed to fetch followers ' , error);
+})
+console.log('Followers: ', followersArray);
+
+followersArray.forEach(githubHandle =>{
+  axios.get('https://api.github.com/users/' + githubHandle)
+  .then(response => {
+    console.log(response.data);
+    document.querySelector('.cards').append(createCard(response.data));
+  })
+.catch(error => {
+  console.log('Fetch operation failed, ', error);
+})
+});
+
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,7 +94,41 @@ const followersArray = [];
 </div>
 
 */
+function createCard(person){
+  const card = document.createElement('div');
+  const img = document.createElement('img');
+  const info = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const address = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
 
+  card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  card.append(img, info);
+  info.append(name, userName, location, profile, followers, following, bio);
+  profile.append(address);
+
+  img.src = person.avatar_url;
+  name.textContent = person.name;
+  userName.textContent = person.login;
+  location.textContent = 'Location: ' + person.location;
+  profile.textContent = 'Profile: ';
+  address.href = person.html_url;
+  address.textContent = person.html_url;
+  followers.textContent = 'Followers: ' + person.followers;
+  following.textContent = 'Following: ' + person.following;
+  bio.textContent = 'Bio: ' + person.bio;
+
+  return card;
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
